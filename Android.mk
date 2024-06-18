fb5118db73d628883759435988c31269b14918f4
fjrXTR <fajarslebew31@gmail.com>
2024-06-18 16:21:54 +0000

    X920: Import Gatekeeper stack
    Change-Id: I4b49e5211b48ae76d18f6ee6e5199b58af633507
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,23 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),X920)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+VENDOR_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib/hw \
+    $(TARGET_OUT_VENDOR)/lib64/hw
+
+$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Making vendor symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+	@ln -sf libMcGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.trustonic.so
+	@ln -sf libMcGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.trustonic.so
+	@ln -sf kmsetkey.trustonic.so $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.default.so
+	@ln -sf kmsetkey.trustonic.so $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.default.so
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
+
 endif
